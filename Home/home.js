368a8fcd4f862ace051d006ecc45c83d4e67f0ad
AnaMaria0214 <ana.and.black@hotmail.com>
2024-04-18 00:20:43 -0500

    home except cards

--- Home/home.js
+++ Home/home.js
@@ -1,20 +1,39 @@
 const navbar = document.querySelector(".nav");
+const footer = document.querySelector(".footer");
 
 document.addEventListener("DOMContentLoaded", () => {
   navbar.innerHTML = `
-    <a href="#" class="nav_Logo">
-        <img src="media/Logo.jpeg" alt="logo" class="LogoImage" />
+    <a href="../About/aboutMe" class="nav_Avatar">
+        <img src="media/avatarAna.svg" alt="avatar" class="avatar" />
       </a>
 
-      <ul class="nav__menu">
+      <ul class="nav_Menu">
+      <div class="box">
+      <li class="nav_section">
+      <a href="">Home</a>
+    </li>
+    </div>
+        
+        <div class="box">
         <li class="nav_section">
-          <a href="">Inicio</a>
+          <a href="">About me</a>
         </li>
+        </div>
+        
+        <div class="box">
         <li class="nav_section">
-          <a href="">Sobre mi</a>
-        </li>
-        <li class="nav_section">
-          <a href="">Contactactame</a>
-        </li>
-      </ul>`;
+        <a href="">Contact me</a>
+      </li>
+      </div>
+        
+      </ul>
+      
+    `;
+  footer.innerHTML = `
+  <footer>
+  <div class="footer-content">
+<p class="unlimited-text">WE ARE UNLIMITED ✨</p>
+<p>© 2024 Anresq. All rights reserved.</p>
+</div>
+</footer>`;
 });
